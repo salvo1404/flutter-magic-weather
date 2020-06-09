7390d829b61a6e70bf7a1e1dc56ec0797a34ad20
Salvatore Balzano <salvatore.balzano@carsguide.com.au>
2020-06-09 16:56:50 +1000

    stateful implemetation for weather widget

--- lib/widgets/weather_widget.dart
+++ lib/widgets/weather_widget.dart
@@ -4,22 +4,29 @@ import 'package:MagicWeather/utils/converters.dart';
 import 'package:intl/intl.dart';
 import 'package:MagicWeather/widgets/forecast_horizontal_widget.dart';
 
-class WeatherWidget extends StatelessWidget {
+class WeatherWidget extends StatefulWidget {
   final Weather weather;
-
   WeatherWidget({this.weather}) : assert(weather != null);
 
   @override
+  _WeatherWidget createState() { return _WeatherWidget(); }
+}
+
+class _WeatherWidget extends State<WeatherWidget> {
+
+  // selected's value = 0. For default first item is open.
+  int selected = 0; //attention
+
+  @override
   Widget build(BuildContext context) {
-    List<int> days = [0,1,2];
+    List<int> days = [0,1,2,4,5];
     var today = new DateTime.now();
-    var expanded = false;
-    return Center(
+    return SingleChildScrollView(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Text(
-            this.weather.cityName,
+            widget.weather.cityName,
             style: TextStyle(
                 fontWeight: FontWeight.w900,
                 letterSpacing: 5,
@@ -30,7 +37,7 @@ class WeatherWidget extends StatelessWidget {
             height: 20,
           ),
           Text(
-            this.weather.description.toUpperCase(),
+            widget.weather.description.toUpperCase(),
             style: TextStyle(
                 fontWeight: FontWeight.w500,
                 letterSpacing: 5,
@@ -41,7 +48,7 @@ class WeatherWidget extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Icon(
-                this.weather.getIconData(),
+                widget.weather.getIconData(),
                 color: Colors.white,
                 size: 70,
               ),
@@ -49,7 +56,7 @@ class WeatherWidget extends StatelessWidget {
                 width: 40,
               ),
               Text(
-                '${this.weather.temperature.as(TemperatureUnit.celsius).round()}°',
+                '${widget.weather.temperature.as(TemperatureUnit.celsius).round()}°',
                 style: TextStyle(
                     fontSize: 100,
                     fontWeight: FontWeight.w100,
@@ -61,58 +68,88 @@ class WeatherWidget extends StatelessWidget {
           
           // WeatherSwipePager(weather: weather),
           Padding(
-            padding: EdgeInsets.all(10),
+            padding: EdgeInsets.all(0),
           ),
           
           
           ListView.builder(
+            key: Key('builder ${selected.toString()}'),
             scrollDirection: Axis.vertical,
             shrinkWrap: true,
             itemCount: days.length,
-            padding: EdgeInsets.only(top: 0, bottom: 0),
+            padding: EdgeInsets.only(top: 10, bottom: 10),
             itemBuilder: (context, index) {
               final day = days[index];
-              if(index == 0) {
-                expanded = true;
-              } else {
-                expanded = false;
-              }
+
+              // Hack to remove ExpansiontTile divider colours
+              final theme = Theme.of(context).copyWith(dividerColor: Colors.transparent);
+
               return Column(
                 children: <Widget>[
                   
-                  ExpansionTile(
-                    title: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          DateFormat('EEEE').format(today.add(new Duration(days: day))),
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                              fontWeight: FontWeight.w900,
-                              letterSpacing: 3,
-                              color: Colors.white,
-                              fontSize: 15,),
+                  Theme(
+                    data: theme,
+                    child: ExpansionTile(
+                        key: Key(index.toString()), //attention 
+                        title: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              DateFormat('EEEE').format(today.add(new Duration(days: day))),
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w900,
+                                  letterSpacing: 3,
+                                  color: Colors.white,
+                                  fontSize: 15,),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            if(index == 0)
+                            Text(
+                              'Today',
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  letterSpacing: 3,
+                                  color: Colors.white,
+                                  fontSize: 15,),
+                            ),
+                          ],
                         ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                      ],
-                    ),
-                    initiallyExpanded: expanded,
-                    trailing: SizedBox(
-                        width: 5,
+                        initiallyExpanded: index==selected,
+                        onExpansionChanged: ((newState){
+                          print('index = $index');
+                          print('selected before = $selected');
+                          
+                          if(newState)
+                              setState(() {
+                              // selected = index;   
+                          });
+                          else setState(() {
+                              // selected = 0; 
+                            });
+
+                          print('selected = $selected');
+                        }),
+                        trailing: SizedBox(
+                            width: 5,
+                          ),
+                        children: <Widget>[
+                          Divider(
+                            thickness : 1,
+                            color:
+                                Colors.white,
+                          ),
+                          ForecastHorizontal(weathers: widget.weather.forecast),
+                          Divider(
+                            thickness : 1,
+                            color:
+                                Colors.white,
+                          ),
+                        ],
                       ),
-                    children: <Widget>[
-                      Divider(
-                        color:
-                            Colors.white,
-                      ),
-                      ForecastHorizontal(weathers: weather.forecast),
-                      Divider(
-                        color:
-                            Colors.white,
-                      ),
-                    ],
                   ),
 
                 ],
